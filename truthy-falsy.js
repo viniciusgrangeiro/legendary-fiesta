5aabc749a71f25be3d3758c2b8bceb18a388bf18
viniciusgrangeiro <vinicius.grangeiro@gmail.com>
2022-08-09 12:35:14 -0300

    os primeiros codigos em javascript

---
+++ truthy-falsy.js
@@ -0,0 +1,28 @@
+// const usuarioLogado = true;
+// const contaPaga = false;
+
+// truthy ou falsy
+
+// 0, "" -> false
+// 1 -> true
+
+// console.log(1 == true) // true
+
+// console.log(0 == false) // true
+// console.log("" == false) // true 
+
+// undefined -> indefinido
+// null -> vazio ou nada
+
+// let varUndefined;
+// let varNull = null;
+
+// console.log(varUndefined);
+// console.log(varNull);
+
+let numero = 3;
+let texto = 'texto';
+
+console.log(typeof numero);
+console.log(typeof texto);
+
