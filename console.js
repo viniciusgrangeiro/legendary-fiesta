8150f7133460f09bbeca715748f59dd0fd63c02d
viniciusgrangeiro <vinicius.grangeiro@gmail.com>
2022-08-09 23:10:09 -0300

    alguns exemplos de console

---
+++ console.js
@@ -0,0 +1,13 @@
+// log -> registro
+
+const minhaVar = true;
+
+// console.log(345);
+// console.log('texto');
+// console.log(minhaVar);
+
+// tratamento erro
+
+
+console.error('deu erro!');
+console.log(1 /0);
